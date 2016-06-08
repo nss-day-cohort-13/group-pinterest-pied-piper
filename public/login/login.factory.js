39aaac9ff9d9ecc90b1ce7ca767a3327870aee46
Joseph Kane <josephckane@gmail.com>
2016-06-08 10:12:05 -0500

    get auth token back, login function fires twice

--- public/login/login.factory.js
+++ public/login/login.factory.js
@@ -1,17 +1,31 @@
 angular.module('app')
 	.factory('LoginFactory', () => {
+		let userId;
+		let	token;
 
-	firebase.auth().onAuthStateChanged((userObj) => {
-		console.log("userObj: ", userObj);
+	firebase.auth().onAuthStateChanged((user) => {
+		console.log("user: ", user);
+		if (user) {
+			userId = user.uid;
+			user.getToken()
+				.then(t => token = t)
+				.then(console.log("token: ", token))
+		}
 	})
 
 		return {
 			login (email, password) {
 				firebase.auth().signInWithEmailAndPassword(email, password)
+
 			},
 
 			register (email, password) {
 				firebase.auth().createUserWithEmailAndPassword(email, password)
+
+			},
+
+			getUserData () {
+				return {user: userId, authToken: token};
 			}
 		}
 	})
