76cf2d60666a903c3f0c7a60ff90c8b9ac70f729
Jessica Wynn <Jessicashinjo@gmail.com>
2016-06-08 14:06:28 -0500

    reworked factory file in order to add other functions

--- public/pins/pin.factory.js
+++ public/pins/pin.factory.js
@@ -1,5 +1,4 @@
 angular.module('app')
-  .constant('firebase_URL', 'https://pied-piper-5ad6b.firebaseio.com/')
   .factory('pinsFactory', ($http, firebase_URL) => {
     let pinList = null;
     return {
